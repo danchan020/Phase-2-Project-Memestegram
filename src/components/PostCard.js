6f18bce74ad3eaf157f6b2ed640a8a9fd8e306b7
Daniel <danchan020@gmail.com>
2022-06-01 09:28:19 -0700

    comment toggle

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Comments from './Comments'
 
 function ContentCard({ id, url, genre, name, comments, favorites, addtoFavorites }) {
-	
+
 	const heartLinkFull = "https://cdn-images-1.medium.com/max/800/1*km6tQMVzzuccuhE0MxvSzQ.png"
 	const heartLink = "https://cdn-images-1.medium.com/max/800/1*vtFx84vFOMGI76SfQTGCVQ.png"
 
@@ -13,10 +13,19 @@ function ContentCard({ id, url, genre, name, comments, favorites, addtoFavorites
 		return <Comments key={comment[index]} comment={comment} />
 	})
 
+	const activeComments =
+		<div>
+			{renderComments}
+			<form>
+				<input></input>
+				<button type="submit">Add Comment</button>
+			</form>
+		</div>
+
 	const handleLike = () => {
 		setIsHeart(!isHeart)
 		console.log(favorites)
-		return isHeart===true ? addtoFavorites(id) : null
+		return isHeart === true ? addtoFavorites(id) : null
 	}
 
 	return (
@@ -32,11 +41,8 @@ function ContentCard({ id, url, genre, name, comments, favorites, addtoFavorites
 				View All Comments
 			</button>
 			<div>
-				{showComments ? renderComments : null}
-				<form>
-					<input></input>
-					<button type="submit">Add Comment</button>
-				</form>
+				{showComments ? activeComments : null}
+
 			</div>
 		</div>
 	)
