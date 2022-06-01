b40eb0b5e33b9db07493249084d8b0921cc0c8d6
Kevin Nielsen <knielsen24@gmail.com>
2022-05-31 20:18:26 -0700

    show comments button

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Comments from './Comments'
 
 function ContentCard({ id, url, genre, name, comments }) {
+
+	const [showComments, setShowComments] = useState(false)
+
+	const renderComments = comments.map((comment, index) => {
+		return <Comments key={comment[index]} comment={comment} />
+	})
+
 	return (
 		<div>
-			<img src={url} alt={name}/>
+			<img src={url} alt={name} />
 			<h5>{name}</h5>
 			<p>{genre}</p>
-			<p>{comments}</p>
+			<button onClick={() => setShowComments(!showComments)} >
+				View All Comments
+			</button>
+			<div>
+				{showComments ? renderComments : null}
+			</div>
 		</div>
 	)
 }
